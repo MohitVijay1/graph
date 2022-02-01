3cd4049b400922d43957844601874c5b07b391e1
Mohit Vijay <54172618+coder-mohit@users.noreply.github.com>
2022-02-01 14:14:34 +0530

    Update bfs.cpp

--- bfs.cpp
+++ bfs.cpp
@@ -1,21 +1,42 @@
-void bfsofgraph(int node,vector<int> adj[],int src)
-{
-    vector<bool> vis(node+1,0);
-    queue<int>q;
-    q.push(src);
-    vis[src]=true;
-    while(!q.empty())
-    {
-        int lr=q.front();
-        cout<<lr<<" ";
-        q.pop();
-        for(auto it:adj[lr])
+#include<bits/stdc++.h>
+using namespace std;
+
+int vertex;
+int edges;
+
+void bfs(int src,vector<int> adj[]){
+ queue<int>q;
+ q.push(src);
+ vector<bool> vis(vertex+1,0);
+ vis[src]=1;
+ while(!q.empty())
+ {
+ cout<<q.front()<<" ";
+    int index=q.front();
+    q.pop();
+    for(auto it:adj[index]){
+        if(vis[it]==0)
         {
-            if(!vis[it])
-            {
-                q.push(it);
-                vis[it]=true;
-            }
+            q.push(it);
+            vis[it]=1;
         }
     }
+ }
+
+
 }
+int main()
+{
+    cin>>vertex;
+   cin>>edges;
+vector<int> adj[vertex+1];
+
+    for(auto i=0;i<edges;i++ )
+    {
+        int u,v;cin>>u>>v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+  bfs(0,adj);   
+  
+  }
