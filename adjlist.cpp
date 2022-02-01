a271827c9634fb7724b927cabd16ad25a4596336
Mohit Vijay <54172618+coder-mohit@users.noreply.github.com>
2022-02-01 14:03:03 +0530

    Update adjlist.cpp

--- adjlist.cpp
+++ adjlist.cpp
@@ -1,10 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int vertex;cin>>vertex;
+    int edges;cin>>edges;
+    vector<int> adj[vertex+1];
+    for(auto i=0;i<edges;i++ )
+    {
+        int u,v;cin>>u>>v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+     for(int i=0;i<vertex;i++)
+     {
+        cout<<i<<"->"<<" ";
+        for(auto it:adj[i]){
+            cout<<it<<" ";
+        }
+        cout<<endl;
+     }
+
+  }
+
+//----------------------------------------------------------------------------------------------------
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
    
 //adjaceny list of bidirectional graph
 //space complexixty=2*edges
-    int n;cin>>n;
+    int n;cin>>n;//n=vertex
     int m;cin>>m;//m==edges
     std::vector<int> adj[n+1];
     for(auto i=0;i<m;i++)
